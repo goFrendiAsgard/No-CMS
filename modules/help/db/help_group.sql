d51f9a4116ec5f3e7dab62c6050cb99c4520351d
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2012-08-07 08:46:59 +0700

    better help module

---
+++ modules/help/db/help_group.sql
@@ -0,0 +1,9 @@
+INSERT INTO `help_group` (`id`, `name`, `content`) VALUES
+(1, 'Basic Info', 'This section contains basic info of No-CMS'),
+(2, 'Installation', 'This section contains installation of No-CMS'),
+(3, 'Getting Started', 'Fasten your seatbelt and get started'),
+(4, 'More With Module', 'More advance topics about modules'),
+(5, 'More With Theme', 'More advance topics about theme'),
+(6, 'Module Generator', 'Make your life easier with Module Generator'),
+(7, 'No-CMS Advance Topic', '<p>\n  No-CMS advance topics</p>\n'),
+(8, 'FAQ', '<p>\n   Frequently Asked Questions</p>\n');
