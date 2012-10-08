21641e4e79cbf07158ab706a5dea7ce32c1e73a6
goFrendiAsgard <goFrendiAsgard@gmail.com>
2012-10-09 05:33:05 +0700

    fix active/inactive link on grid

--- modules/main/assets/scripts/navigation.js
+++ modules/main/assets/scripts/navigation.js
@@ -1,19 +1,21 @@
-$(".navigation_active").click(function(){
-	var str = $(this).html();
-	var $this = $(this);
-	$.ajax({
-		url: $(this).attr('target'),
-		dataType: 'json',
-		success: function(response){					
-			if(str == 'Active'){
-				str = 'Inactive';
-			}else{
-				str = 'Active';
+$(document).ready(function(){
+	$(".navigation_active").click(function(){
+		var str = $(this).html();
+		var $this = $(this);
+		$.ajax({
+			url: $(this).attr('target'),
+			dataType: 'json',
+			success: function(response){					
+				if(str == 'Active'){
+					str = 'Inactive';
+				}else{
+					str = 'Active';
+				}
+				if(response.success){
+					console.log(str);
+					$this.html(str);
+				}
 			}
-			if(response.success){
-				console.log(str);
-				$this.html(str);
-			}
-		}
+		});
 	});
 });
--- modules/main/assets/scripts/widget.js
+++ modules/main/assets/scripts/widget.js
@@ -1,19 +1,21 @@
-$(".widget_active").click(function(){
-    var str = $(this).html();
-    var $this = $(this);
-    $.ajax({
-        url: $(this).attr('target'),
-        dataType: 'json',
-        success: function(response){                    
-            if(str == 'Active'){
-                str = 'Inactive';
-            }else{
-                str = 'Active';
-            }
-            if(response.success){
-                console.log(str);
-                $this.html(str);
-            }
-        }
-    });
+$(document).ready(function(){
+	$(".widget_active").click(function(){
+	    var str = $(this).html();
+	    var $this = $(this);
+	    $.ajax({
+	        url: $(this).attr('target'),
+	        dataType: 'json',
+	        success: function(response){                    
+	            if(str == 'Active'){
+	                str = 'Inactive';
+	            }else{
+	                str = 'Active';
+	            }
+	            if(response.success){
+	                console.log(str);
+	                $this.html(str);
+	            }
+	        }
+	    });
+	});
 });
