11db3dce916293728da55b40f3cfc5fe7c2d569e
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2012-12-04 22:35:45 +0700

    rollback to the previous code

--- assets/grocery_crud/js/jquery_plugins/config/jquery.ckeditor.config.js
+++ assets/grocery_crud/js/jquery_plugins/config/jquery.ckeditor.config.js
@@ -1,3 +1,8 @@
 $(function(){
-	$( 'textarea.texteditor' ).ckeditor();
+	$( 'textarea.texteditor' ).ckeditor({toolbar:'Full'});
+	$( 'textarea.mini-texteditor' ).ckeditor({toolbar:'Basic',width:700});
+	$( 'textarea.texteditor' ).ckeditor({
+        baseHref : "/~gofrendi/No-CMS/", 
+        baseUrl  : "/~gofrendi/No-CMS/",
+    });
 });
