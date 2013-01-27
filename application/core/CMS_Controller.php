1fe82ca0c4963408f6ef2d8c2635cbdb3884bbc9
goFrendiAsgard <goFrendiAsgard@gmail.com>
2013-01-27 13:56:07 +0700

    surpress error on ob_start()

--- application/core/CMS_Controller.php
+++ application/core/CMS_Controller.php
@@ -642,7 +642,7 @@ class CMS_Controller extends MX_Controller {
      * @desc    show variable for debugging purpose
      */
     protected final function cms_show_variable($variable){
-    	ob_start();
+    	@ob_start();
     	echo '<pre>';
 		echo var_dump($variable);
     	echo '</pre>';
@@ -655,7 +655,7 @@ class CMS_Controller extends MX_Controller {
      * @desc    you are encouraged to use this instead of echo $html
      */
     protected final function cms_show_html($html){
-    	ob_start();
+    	@ob_start();
     	echo $html;
 		@ob_end_flush();
     }
