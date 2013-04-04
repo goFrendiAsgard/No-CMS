50ab8a7d731c0c43ddcb1b355410bb25a14eea3e
GoFrendi Gunawan <gofrendiasgard@gmail.com>
2013-04-04 20:47:26 +0700

    fix themes bug

--- themes/cerulean/views/layouts/default.php
+++ themes/cerulean/views/layouts/default.php
@@ -2,27 +2,27 @@
 <html lang="{{ language:language_alias }}">
   <head>
     <meta charset="utf-8">
-    <title><?php echo $template['title'];?></title> 
-    <?php echo $template['metadata'];?>   
+    <title><?php echo $template['title'];?></title>
+    <?php echo $template['metadata'];?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    
+
     <link rel="icon" href="{{ site_favicon }}">
 
     <!-- Le styles -->
-    <?php 
+    <?php
         $asset = new CMS_Asset();
         $asset->add_themes_css('style.css', $cms['site_theme'], 'default');
         $asset->add_themes_css('bootstrap.min.css', $cms['site_theme'], 'default');
         $asset->add_themes_css('bootstrap-responsive.min.css', $cms['site_theme'], 'default');
-        echo $asset->compile_css();     
-        
+        echo $asset->compile_css();
+
         $asset->add_cms_js("bootstrap/js/bootstrap.min.js");
         $asset->add_themes_js('script.js', $cms['site_theme'], 'default');
-        echo $asset->compile_js(TRUE);      
+        echo $asset->compile_js(TRUE);
     ?>
-    
+
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -41,39 +41,39 @@
                 <span class="icon-bar"></span>
             </a>
             <a class="brand" href="#">
-                <img src ="{{ site_logo }}" style="max-height:20px; max-width:20px;" />             
-            </a>          
-            <div class="nav-collapse in collapse" id="main-menu" style="height: auto; ">
+                <img src ="{{ site_logo }}" style="max-height:20px; max-width:20px;" />
+            </a>
+            <div class="nav-collapse in collapse" id="main-menu">
                 {{ navigation_top_quicklink }}
             </div>
-        </div>       
+        </div>
       </div>
     </div>
-    
+
     <div class="container">
       <div class="row-fluid">
         <div id="layout-banner" class="well hidden-phone span12">
             <div class="span2">
-                <img src ="{{ site_logo }}" /> 
-            </div>          
+                <img src ="{{ site_logo }}" />
+            </div>
             <div class="span10">
                 <h1>{{ site_name }}</h1>
                 <p>{{ site_slogan }}</p>
-            </div>                         
-        </div>      
+            </div>
+        </div>
         <div id="layout-content" class="span9">
             <div>{{ navigation_path }}</div><hr />
             <?php echo $template['body'];?>
             <div class="clear"></div>
-        </div><!--/#layout-content-->       
-        <div id="layout-widget" class="span2">                  
+        </div><!--/#layout-content-->
+        <div id="layout-widget" class="span2">
             <h4>WIDGET</h4><hr />{{ widget:sidebar }}
-            <h4>ADVERTISEMENT</h4><hr />{{ widget:advertisement }}      
-        </div><!--/#layout-widget-->              
+            <h4>ADVERTISEMENT</h4><hr />{{ widget:advertisement }}
+        </div><!--/#layout-widget-->
       </div><!--/row-->
       <hr>
       <footer>{{ site_footer }}</footer>
     </div><!--/.fluid-container-->
-    
+
   </body>
 </html>
--- themes/minimal/views/layouts/default.php
+++ themes/minimal/views/layouts/default.php
@@ -2,26 +2,26 @@
 <html lang="{{ language:language_alias }}">
   <head>
     <meta charset="utf-8">
-    <title><?php echo $template['title'];?></title> 
-    <?php echo $template['metadata'];?>   
+    <title><?php echo $template['title'];?></title>
+    <?php echo $template['metadata'];?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    
+
     <link rel="icon" href="{{ site_favicon }}">
 
     <!-- Le styles -->
-    <?php 
+    <?php
         $asset = new CMS_Asset();
         $asset->add_themes_css('style.css', $cms['site_theme'], 'default');
-        $asset->add_cms_css('bootstrap/css/bootstrap.min.css'); 
-        echo $asset->compile_css();     
-        
+        $asset->add_cms_css('bootstrap/css/bootstrap.min.css');
+        echo $asset->compile_css();
+
         $asset->add_cms_js("bootstrap/js/bootstrap.min.js");
         $asset->add_themes_js('script.js', $cms['site_theme'], 'default');
-        echo $asset->compile_js(TRUE);      
+        echo $asset->compile_js(TRUE);
     ?>
-    
+
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -40,23 +40,23 @@
                 <span class="icon-bar"></span>
             </a>
             <a class="brand" href="#">
-                <img src ="{{ site_logo }}" style="max-height:20px; max-width:20px;" />             
-            </a>          
-            <div class="nav-collapse in collapse" id="main-menu" style="height: auto; ">
+                <img src ="{{ site_logo }}" style="max-height:20px; max-width:20px;" />
+            </a>
+            <div class="nav-collapse in collapse" id="main-menu">
                 {{ navigation_top_quicklink }}
             </div>
-        </div>       
+        </div>
       </div>
     </div>
-    
+
     <div class="container">
       <div class="layout-content">
-    	<?php echo $template['body'];?>              
+    	<?php echo $template['body'];?>
       </div><!--/row-->
       <hr>
       <footer>{{ site_footer }}</footer>
     </div><!--/.fluid-container-->
-    
+
   </body>
 </html>
 
