e7deae66c72b109714f17f80421ee5da1b44677d
shakespam <shakespam@gmail.com>
2013-05-29 10:50:19 +0200

    extended translation support

--- modules/main/views/module_management.php
+++ modules/main/views/module_management.php
@@ -13,7 +13,7 @@
        margin-top:10px;
     }
 </style>
-<h3>Module Management</h3>
+<h3>{{ language:Module Management }}</h3>
 <?php
     for($i=0; $i<count($modules); $i++){
         $module = $modules[$i];
@@ -30,10 +30,10 @@
         	echo '<img class="logo" src="'.base_url('assets/nocms/images/icons/package.png').'" />';
         }
         echo '  <br />';
-        echo '  <a id="module_'.$i.'_activate" class="btn btn-mini disabled" href="'.site_url($module['module_path'].'/install/activate').'"><i class="icon-ok"></i>&nbsp;Activate</a>';
-        echo '  <a id="module_'.$i.'_upgrade" class="btn btn-mini disabled" href="'.site_url($module['module_path'].'/install/upgrade').'"><i class="icon-arrow-up"></i>&nbsp;Upgrade</a>';
-        echo '  <a id="module_'.$i.'_deactivate" class="btn btn-danger btn-mini disabled" href="'.site_url($module['module_path'].'/install/deactivate').'"><i class="icon-remove"></i>&nbsp;Deactivate</a>';
-        echo '  <a id="module_'.$i.'_setting" class="btn btn-mini disabled" href="'.site_url($module['module_path'].'/install/setting').'"><i class="icon-wrench"></i>&nbsp;Setting</a>';
+        echo '  <a id="module_'.$i.'_activate" class="btn btn-mini disabled" href="'.site_url($module['module_path'].'/install/activate').'"><i class="icon-ok"></i>&nbsp;{{ language:lang_mm_activate }}</a>';
+        echo '  <a id="module_'.$i.'_upgrade" class="btn btn-mini disabled" href="'.site_url($module['module_path'].'/install/upgrade').'"><i class="icon-arrow-up"></i>&nbsp;{{ language:lang_mm_upgrade }}</a>';
+        echo '  <a id="module_'.$i.'_deactivate" class="btn btn-danger btn-mini disabled" href="'.site_url($module['module_path'].'/install/deactivate').'"><i class="icon-remove"></i>&nbsp;{{ language:lang_mm_deactivate }}</a>';
+        echo '  <a id="module_'.$i.'_setting" class="btn btn-mini disabled" href="'.site_url($module['module_path'].'/install/setting').'"><i class="icon-wrench"></i>&nbsp;{{ language:lang_mm_settings }}</a>';
         echo '</div>';
         echo '<div class="span8">';
         echo '  <br />';
@@ -49,24 +49,24 @@
         echo '          var status = "";';
         echo '          if(response.active){';
         echo '              if(response.old){';
-        echo '                  status += "Need upgrade";';
+        echo '                  status += "{{ language:lang_mm_need_upgrade }}";';
         echo '                  $("#module_'.$i.'_upgrade").removeClass("disabled");';
         echo '              }else{';
-        echo '                  status += "Active";';
+        echo '                  status += "{{ language:lang_mm_active }}";';
         echo '                  $("#module_'.$i.'_setting").removeClass("disabled");';
         echo '                  $("#module_'.$i.'_deactivate").removeClass("disabled");';
         echo '              }';
         echo '          }else{';
-        echo '              status += "Inactive";';
+        echo '              status += "{{ language:lang_mm_inactive }}";';
         echo '              $("#module_'.$i.'_setting").removeClass("disabled");';
         echo '              $("#module_'.$i.'_activate").removeClass("disabled");';
         echo '          }';
         echo '          var html = "";';
         echo '          html += "("+response.name+")<br />";';
         echo '          html += "<p>"+response.description+"</p>";';
-        echo '          html += "<strong>Registered Version</strong> : "+response.old_version+" | ";';
-        echo '          html += "<strong>Current Version</strong> : "+response.version+" | ";';
-        echo '          html += "<strong>Status</strong> : "+status;';
+        echo '          html += "<strong>{{ language:lang_mm_registered_version }}</strong> : "+response.old_version+" | ";';
+        echo '          html += "<strong>{{ language:lang_mm_current_version }}</strong> : "+response.version+" | ";';
+        echo '          html += "<strong>{{ language:lang_mm_status }}</strong> : "+status;';
         echo '          ';
         echo '          $("#div_module_'.$i.'_info").html(html);';
         echo '      }';
@@ -77,7 +77,7 @@
 	echo '<div style="clear:both"></div>';
     if($upload['uploading'] && !$upload['success']){
     	echo '<div id="message" class="alert alert-error">';
-    	echo '<b>Error:</b> '.$upload['message'];
+    	echo '<b>{{ language:lang_mm_error }}:</b> '.$upload['message'];
     	echo '</div>';
     }
 ?>
@@ -85,7 +85,7 @@
 	<form action="<?php echo site_url('main/module_management');?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 		<input type="file" name="userfile" size="20" />
 		<br /><br />
-		<input name="upload" class="btn btn-primary" type="submit" value="Upload New Module" />
+		<input name="upload" class="btn btn-primary" type="submit" value="<?php echo $this->No_CMS_Model->cms_lang('lang_mm_upload');?>" />
 	</form>
 </div>
 
