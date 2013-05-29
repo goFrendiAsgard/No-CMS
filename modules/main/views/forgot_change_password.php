156eac0c67ad682f826cbb72270bb75094e9b7f8
shakespam <shakespam@gmail.com>
2013-05-29 10:43:41 +0200

    extended translation support

--- modules/main/views/forgot_change_password.php
+++ modules/main/views/forgot_change_password.php
@@ -1,8 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+    echo '<h3>{{ language:Forgot Password }}</h3>';
     echo form_open('main/forgot/'.$activation_code);
-    echo form_label('New Password').br();
+    echo form_label('{{ language:lang_fcp_new }}').br();
     echo form_password('password').br();
-    echo form_label('New Password (again)').br();
+    echo form_label('{{ language:lang_fcp_new_again }}').br();
     echo form_password('confirm_password').br();
-    echo form_submit('change', 'Change');
+    echo form_submit('change', $this->No_CMS_Model->cms_lang('lang_fcp_submit'), 'class="btn btn-primary"');
     echo form_close();
