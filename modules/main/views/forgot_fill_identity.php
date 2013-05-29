898dd8db335642bbb072ad87ecf8045e07fe87b2
shakespam <shakespam@gmail.com>
2013-05-29 10:44:53 +0200

    extended translation support

--- modules/main/views/forgot_fill_identity.php
+++ modules/main/views/forgot_fill_identity.php
@@ -1,6 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+    echo '<h3>{{ language:Forgot Password }}</h3>';
     echo form_open('main/forgot');
-    echo form_label('Identity').br();
+    echo form_label('{{ language:lang_ffi_identity }}').br();
     echo form_input('identity', $identity).br();
-    echo form_submit('send_activation_code', 'Send Activation Code to My E mail');
+    echo form_submit('send_activation_code', $this->No_CMS_Model->cms_lang('lang_ffi_submit'), 'class="btn btn-primary"');
     echo form_close();
