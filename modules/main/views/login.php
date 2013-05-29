9be684a5cbf08804bf7713a7cfe1887a6b1834d8
shakespam <shakespam@gmail.com>
2013-05-29 11:13:01 +0200

    extended translation support

--- modules/main/views/login.php
+++ modules/main/views/login.php
@@ -4,21 +4,23 @@
         display:none;
     }
 </style>
+<h3>{{ language:Login }}</h3>
 <?php
     echo form_open('main/login');
-    echo form_label('Identity');
+    echo form_label('{{ language:lang_login_identity }}');
     echo form_input('identity', $identity).br();
-    echo form_label('Password');
+    echo form_label('{{ language:lang_login_password }}');
     echo form_password('password').br();
-    echo form_submit('login', 'Log In', 'class="btn btn-primary"');
+    echo form_submit('login', $this->No_CMS_Model->cms_lang('lang_login_submit'), 'class="btn btn-primary"');
 	echo '&nbsp';
-	echo anchor(site_url('main/register'),'Register',array('class'=>'btn'));
+	echo anchor(site_url('main/register'),'{{ language:lang_login_register_btn }}',array('class'=>'btn'));
     echo form_close();
+
 	if(count($providers)>0){
-		echo 'Or login with:'.br();
+		echo '{{ language:lang_login_hauth }}:'.br();
 		foreach($providers as $provider=>$connected){
 			echo anchor(site_url('main/hauth/login/'.$provider), '<img src="'.base_url('modules/main/assets/third_party/'.$provider.'.png').'" />');
 		}
-	}	
+	}
 ?>
 <div id="login_message"><?php echo isset($message)?$message:''; ?></div>
