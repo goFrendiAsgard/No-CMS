385a69ac57772f2c005313fb01ccf4cdbfff816f
shakespam <shakespam@gmail.com>
2013-05-29 10:34:22 +0200

    Update english.php
    Extended translations support for main module

--- modules/main/assets/languages/english.php
+++ modules/main/assets/languages/english.php
@@ -1,2 +1,111 @@
 <?php
 $lang['No-CMS User Guide'] = 'No-CMS User Guide';
+
+// forgot_fill_identity.php
+$lang['lang_ffi_identity'] = 'Identity';
+$lang['lang_ffi_submit'] = 'Send Activation Code to My E mail';
+
+// forgot_change_password.php
+$lang['lang_fcp_new'] = 'New Password';
+$lang['lang_fcp_new_again'] = 'New Password (again)';
+$lang['lang_fcp_submit'] = 'Change';
+
+// change_profile.php
+$lang['lang_cp_pw_empty'] = 'Password is empty';
+$lang['lang_cp_pw_no_match'] = 'Confirm password doesn\\\'t match';
+$lang['lang_cp_user_name'] = 'User Name';
+$lang['lang_cp_email'] = 'E mail';
+$lang['lang_cp_real_name'] = 'Real Name';
+$lang['lang_cp_do_change_pw'] = 'Change Password';
+$lang['lang_cp_new_pw'] = 'Password';
+$lang['lang_cp_new_pw_again'] = 'Confirm Password';
+$lang['lang_cp_submit'] = 'Change Profile';
+
+// change_theme.php
+$lang['lang_ct_use'] = 'Use';
+$lang['lang_ct_current'] = 'Currently use';
+$lang['lang_ct_theme'] = 'theme';
+$lang['lang_ct_no_preview'] = 'No Preview.';
+$lang['lang_ct_error'] = 'Error: ';
+$lang['lang_ct_upload_new'] = 'Upload New Theme';
+
+// login.php
+$lang['lang_login_identity'] = 'User Name';
+$lang['lang_login_password'] = 'Password';
+$lang['lang_login_register_btn'] = 'Register';
+$lang['lang_login_hauth'] = 'Or Log In with:';
+$lang['lang_login_submit'] = 'Log In';
+$lang['lang_login_failed'] = 'Login Failed!';
+
+// module_activation_error.php
+$lang['lang_mae_install_failed'] = 'Installation Failed';
+$lang['lang_mae_cannot_activate'] = 'Cannot activate';
+$lang['lang_mae_back'] = 'Back';
+
+// module_deactivation_error.php
+$lang['lang_mde_uninstall_failed'] = 'Uninstallation Failed';
+$lang['lang_mde_cannot_deactivate'] = 'Cannot deactivate';
+
+// module_management.php
+$lang['lang_mm_activate'] = 'Activate';
+$lang['lang_mm_upgrade'] = 'Upgrade';
+$lang['lang_mm_deactivate'] = 'Deactivate';
+$lang['lang_mm_settings'] = 'Settings';
+$lang['lang_mm_need_upgrade'] = 'Need Upgrade';
+$lang['lang_mm_active'] = 'Active';
+$lang['lang_mm_inactive'] = 'Inactive';
+$lang['lang_mm_registered_version'] = 'Registered Version';
+$lang['lang_mm_current_version'] = 'Current Version';
+$lang['lang_mm_status'] = 'Status';
+$lang['lang_mm_error'] = 'Error';
+$lang['lang_mm_upload'] = 'Upload New Module';
+
+// module_upgrade_error.php
+$lang['lang_mue_cannot_upgrade'] = 'Cannot upgrade';
+
+// register.php (most is shared with change_profile.php)
+$lang['lang_reg_title'] = 'New User Registration';
+$lang['lang_reg_submit'] = 'Register';
+$lang['lang_reg_submit'] = 'Register';
+
+
+// rest, grocery crud related (add button, column titles)
+$lang['lang_gc_active'] = 'Active';
+$lang['lang_gc_inactive'] = 'Inactive';
+$lang['lang_gc_groups'] = 'Groups';
+$lang['lang_gc_user'] = 'User';
+$lang['lang_gc_authorization'] = 'Authorization';
+$lang['lang_gc_can_not_delete_admin'] = 'You cannot delete super admin user or your own account';
+$lang['lang_gc_user_group'] = 'User Group';
+$lang['lang_gc_group'] = 'Group';
+$lang['lang_gc_description'] = 'Description';
+$lang['lang_gc_users'] = 'Users';
+$lang['lang_gc_navigations'] = 'Navigations';
+$lang['lang_gc_privileges'] = 'Privileges';
+$lang['lang_gc_can_not_delete_admin_group'] = 'You cannot delete Admin group or group which is not empty, please empty the group first';
+$lang['lang_gc_add_navigation'] = 'Navigation (Page)';
+$lang['lang_gc_navigation_code'] = 'Navigation Code';
+$lang['lang_gc_is_root'] = 'Is Root';
+$lang['lang_gc_parent'] = 'Parent';
+$lang['lang_gc_navigation_title'] = 'Navigation Title (What visitor see)';
+$lang['lang_gc_page_title'] = 'Page Title';
+$lang['lang_gc_page_keyword'] = 'Page Keyword (Comma Separated)';
+$lang['lang_gc_url'] = 'URL (Where is it point to)';
+$lang['lang_gc_static'] = 'Static';
+$lang['lang_gc_static_content'] = 'Static Content';
+$lang['lang_gc_only_content'] = 'Only show content';
+$lang['lang_gc_default_theme'] = 'Default Theme';
+$lang['lang_gc_quick_link'] = 'Quick Link';
+$lang['lang_gc_add_privilege'] = 'Privilege';
+$lang['lang_gc_widget'] = 'Widget';
+$lang['lang_gc_widget_code'] = 'Widget Code';
+$lang['lang_gc_widget_title'] = 'Title (What visitor see)';
+$lang['lang_gc_order'] = 'Order';
+$lang['lang_gc_widget_slug'] = 'Slug';
+$lang['lang_gc_configuration'] = 'Configuration';
+$lang['lang_gc_configuration_key'] = 'Configuration Key';
+$lang['lang_gc_configuration_value'] = 'Configuration Value';
+
+// {{ language: }}
+// $this->No_CMS_Model->cms_lang('')
+// class="btn btn-primary"
