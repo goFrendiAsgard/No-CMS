7ffda8cf851a77fc31bbbad47546460de7745b71
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-10-05 11:50:56 +0700

    remove stiki_ibk

--- modules/stiki_ibk/config/module_config.php
+++
@@ -1,4 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['module_table_prefix']  = 'ibk';
-$config['module_prefix']        = 'stiki_ibk';
--- modules/stiki_ibk/views/stiki_ibk_index.php
+++
@@ -1,3 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<h4>stiki_ibk</h4>
-<?php echo $content; ?>
