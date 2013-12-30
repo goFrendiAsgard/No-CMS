03711a3088be5066a0d4e7e97478e7b9220d5b99
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-12-30 11:55:46 +0700

    try to make only first-level dropdown works

--- modules/main/controllers/main.php
+++ modules/main/controllers/main.php
@@ -1403,56 +1403,56 @@ class Main extends CMS_Controller
                 $result .= $this->build_quicklink();
             }
             $result = 
-            '<style type="text/css">
-                .dropdown-submenu{
-                    position:relative;
-                }
-                 
-                .dropdown-submenu > .dropdown-menu
-                {
-                    top:0;
-                    left:100%;
-                    margin-top:-6px;
-                    margin-left:-1px;
-                    -webkit-border-radius:0 6px 6px 6px;
-                    -moz-border-radius:0 6px 6px 6px;
-                    border-radius:0 6px 6px 6px;
-                }
-                 
-                .dropdown-submenu:hover > .dropdown-menu{
-                    display:block;
-                }
-                 
-                .dropdown-submenu > a:after{
-                    display:block;
-                    content:" ";
-                    float:right;
-                    width:0;
-                    height:0;
-                    border-color:transparent;
-                    border-style:solid;
-                    border-width:5px 0 5px 5px;
-                    border-left-color:#cccccc;
-                    margin-top:5px;
-                    margin-right:-10px;
-                }
-                 
-                .dropdown-submenu:hover > a:after{
-                    border-left-color:#ffffff;
-                }
-                 
-                .dropdown-submenu .pull-left{
-                    float:none;
-                }
-                 
-                .dropdown-submenu.pull-left > .dropdown-menu{
-                    left:-100%;
-                    margin-left:10px;
-                    -webkit-border-radius:6px 0 6px 6px;
-                    -moz-border-radius:6px 0 6px 6px;
-                    border-radius:6px 0 6px 6px;
-                }
+            '<style type="text/css">                
                 @media (min-width: 750px){
+                    .dropdown-submenu{
+                        position:relative;
+                    }
+                     
+                    .dropdown-submenu > .dropdown-menu
+                    {
+                        top:0;
+                        left:100%;
+                        margin-top:-6px;
+                        margin-left:-1px;
+                        -webkit-border-radius:0 6px 6px 6px;
+                        -moz-border-radius:0 6px 6px 6px;
+                        border-radius:0 6px 6px 6px;
+                    }
+                     
+                    .dropdown-submenu:hover > .dropdown-menu{
+                        display:block;
+                    }
+                     
+                    .dropdown-submenu > a:after{
+                        display:block;
+                        content:" ";
+                        float:right;
+                        width:0;
+                        height:0;
+                        border-color:transparent;
+                        border-style:solid;
+                        border-width:5px 0 5px 5px;
+                        border-left-color:#cccccc;
+                        margin-top:5px;
+                        margin-right:-10px;
+                    }
+                     
+                    .dropdown-submenu:hover > a:after{
+                        border-left-color:#ffffff;
+                    }
+                     
+                    .dropdown-submenu .pull-left{
+                        float:none;
+                    }
+                     
+                    .dropdown-submenu.pull-left > .dropdown-menu{
+                        left:-100%;
+                        margin-left:10px;
+                        -webkit-border-radius:6px 0 6px 6px;
+                        -moz-border-radius:6px 0 6px 6px;
+                        border-radius:6px 0 6px 6px;
+                    }
                     .dropdown .caret{
                         display:inline-block!important;
                     }
@@ -1603,7 +1603,7 @@ class Main extends CMS_Controller
                     $html.= '<li class="dropdown">';
                     $html.= '<a class="dropdown-toggle" data-toggle="dropdown" href="'.$quicklink['url'].'">'.
                         '<span class="anchor-text">'.$icon.$quicklink['title'].'</span>'.
-                        '&nbsp;<span class="caret hidden-sm hidden-xs"></span></a>';
+                        '&nbsp;<span class="caret"></span></a>'; // hidden-sm hidden-xs
                     $html.= $this->build_quicklink($quicklink['child'],FALSE);
                     $html.= '</li>';
                 }else{
