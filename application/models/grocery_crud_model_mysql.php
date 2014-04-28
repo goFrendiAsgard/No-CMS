dd687a2e9c9930119953633404beb0995d71a180
goFrendiAsgard <gofrendiasgard@gmail.com>
2014-04-28 19:54:26 +0700

    change mysql & mysqli base to generic model

--- application/models/grocery_crud_model_mysql.php
+++ application/models/grocery_crud_model_mysql.php
@@ -1,4 +1,4 @@
 <?php
-class grocery_crud_model_MySQL extends grocery_CRUD_Model{
-	
+class grocery_crud_model_MySQL extends grocery_CRUD_Generic_Model{
+
 }
--- application/models/grocery_crud_model_mysqli.php
+++ application/models/grocery_crud_model_mysqli.php
@@ -1,4 +1,4 @@
 <?php
-class grocery_crud_model_MySQLi extends grocery_CRUD_Model{
-	
+class grocery_crud_model_MySQLi extends grocery_CRUD_Generic_Model{
+
 }
