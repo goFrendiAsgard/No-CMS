c7999522fe45619f3b86730605cdc53ba830dcaa
Laszlo Espadas <kw@kardiweb.org>
2014-08-02 17:02:46 +0200

    Add hungarian language by Laszlo Espadas

---
+++ modules/main/assets/languages/hungarian.php
@@ -0,0 +1,107 @@
+<?php
+// forgot_fill_identity.php
+$lang['Identity'] = 'Felhasználónév';
+$lang['Send activation code to my email'] = 'Aktivákó kód küldése emailben';
+
+// forgot_change_password.php
+$lang['New Password'] = 'Új jelszó';
+$lang['New Password (again)'] = 'Új jelszó (Újra)';
+$lang['Change'] = 'Változtatás';
+
+// change_profile.php
+$lang['Password is empty'] = 'A jelszó üres';
+$lang['Confirm password doesn\'t match'] = 'A jelszó nem egyezik';
+$lang['User Name'] = 'Felhasználónév';
+$lang['Email'] = 'Email';
+$lang['Real Name'] = 'Valódi név';
+$lang['Change Password'] = 'Jelszó változtatás';
+$lang['Password'] = 'Jelszó';
+$lang['Confirm Password'] = 'Jelszó megerősítés';
+$lang['Change Profile'] = 'Adatmódosítás';
+
+// change_theme.php
+$lang['Use'] = 'Használat';
+$lang['Currently use theme'] = 'Jelenleg használt sablon';
+$lang['Use theme'] = 'Sablon használat';
+$lang['No Preview'] = 'Nincs előnézet.';
+$lang['Error'] = 'Hiba';
+$lang['Upload New Theme'] = 'Új sablon feltöltés';
+
+// login.php
+$lang['User Name'] = 'Felhasználónév';
+$lang['Password'] = 'Jelszó';
+$lang['Register'] = 'Regisztráció';
+$lang['Or Login with'] = 'vagy belépés';
+$lang['Login'] = 'Belépés';
+$lang['Login Failed'] = 'Belépés hiba!';
+
+// module_activation_error.php
+$lang['Installation Failed'] = 'Telepítés hiba';
+$lang['Cannot activate'] = 'Nem lehet aktiválni';
+$lang['Back'] = 'Vissza';
+
+// module_deactivation_error.php
+$lang['Uninstallation Failed'] = 'Eltávolítás hiba';
+$lang['Cannot deactivate'] = 'Nem lehet eltávolítani';
+
+// module_management.php
+$lang['Activate'] = 'Aktiválás';
+$lang['Upgrade'] = 'Frissítés';
+$lang['Deactivate'] = 'Kikapcsolás';
+$lang['Settings'] = 'Beállítások';
+$lang['Need Upgrade'] = 'Frissítés szükséges';
+$lang['Active'] = 'Elérhető';
+$lang['Inactive'] = 'Nem elérhető';
+$lang['Registered Version'] = 'Regisztrált verzió';
+$lang['Current Version'] = 'Jelenlegi verzió';
+$lang['Status'] = 'Állapot';
+$lang['Error'] = 'Hiba';
+$lang['Just another module'] = 'Csak egy újabb modul';
+$lang['Upload New Module'] = 'Új modul feltöltés';
+
+// module_upgrade_error.php
+$lang['Cannot upgrade'] = 'Nem lehet frissíteni';
+
+// register.php (most is shared with change_profile.php)
+$lang['New User Registration'] = 'Új felhasználó regisztráció';
+$lang['Register'] = 'Regisztráció';
+
+
+// rest, grocery crud related (add button, column titles)
+$lang['Active'] = 'Elérhető';
+$lang['Inactive'] = 'Nem elérhető';
+$lang['Groups'] = 'Csoportok';
+$lang['User'] = 'Felhasználó';
+$lang['Authorization'] = 'Hitelesítés';
+$lang['You cannot delete super admin user or your own account'] = 'Nem lehet törölni az adminisztrátor felhasználót vagy a saját fiókot.';
+$lang['User Group'] = 'Felhasználói csoport';
+$lang['Group'] = 'Csoport';
+$lang['Description'] = 'Leírás';
+$lang['Users'] = 'Felhasználók';
+$lang['Navigations'] = 'Menük';
+$lang['Privileges'] = 'Jogok';
+$lang['Title'] = 'Cím';
+$lang['Privilege Code'] = 'Jog kód';
+$lang['You cannot delete admin group or group which is not empty, please empty the group first'] = 'Nem lehet törölni az admin csoportot addig amíg nem üres. Töröljük a felhasználókat a csoportból és a törlés megkezdhető.';
+$lang['Navigation (Page)'] = 'Menü (Oldal)';
+$lang['Navigation Code'] = 'Menü kőd';
+$lang['Is Root'] = 'Is Root';
+$lang['Parent'] = 'Szülő';
+$lang['Navigation Title (What visitor see)'] = 'Menü cím (Ki láthatja)';
+$lang['Page Title'] = 'Oldal cím';
+$lang['Page Keyword (Comma Separated)'] = 'Oldal kulcsszó (vesszővel elválasztva)';
+$lang['URL (Where it is point to)'] = 'Hivatkozás (Amennyiben szükséges)';
+$lang['Static'] = 'Statikus';
+$lang['Static Content'] = 'Statikus tartalom';
+$lang['Only show content'] = 'Csak tartalom megtekintés';
+$lang['Default Theme'] = 'Alapértelmezett sablon';
+$lang['Quick Link'] = 'Gyors hivatkozás';
+$lang['Privilege'] = 'Jog';
+$lang['Widget'] = 'Widget';
+$lang['Widget Code'] = 'Widget kód';
+$lang['Title (What visitor see)'] = 'Cím (Ki láthatja)';
+$lang['Order'] = 'Rendezés';
+$lang['Slug'] = 'Keresőbarát név';
+$lang['Configuration'] = 'Beállítások';
+$lang['Configuration Key'] = 'Beállítások kulcs';
+$lang['Configuration Value'] = 'Beállítások érték';
