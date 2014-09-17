d9e46612dd5a65b76797c3e7befbf35ce4f298a3
goFrendiAsgard <gofrendiasgard@gmail.com>
2014-09-17 12:41:30 +0700

    remove unused module

--- modules/breast_cancer/config/module_config.php
+++
@@ -1,4 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['module_table_prefix']  = 'bc';
-$config['module_prefix']        = 'breast_cancer';
--- modules/breast_cancer/views/breast_cancer_index.php
+++
@@ -1,3 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<h4>breast_cancer</h4>
-<?php echo $content; ?>
