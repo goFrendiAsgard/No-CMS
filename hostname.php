5fab0f4ad069a730933a6889f38083837adfabd8
goFrendiAsgard <gofrendiasgard@gmail.com>
2014-11-21 12:59:11 +0700

    delete hostname.php

--- hostname.php
+++
@@ -1,2 +0,0 @@
-<?php
-$hostname = "gofrendi.dev";
--- reset-installation.sh
+++ reset-installation.sh
@@ -9,7 +9,7 @@ sudo rm -f ./assets/kcfinder/config.php
 sudo rm -Rf ./assets/kcfinder/upload/main-*
 sudo rm -Rf ./assets/kcfinder/upload/site-*
 sudo rm -f ./.htaccess
-sudo rm -f ./.hostname.php
+sudo rm -f ./hostname.php
 sudo rm -Rf ./application/config/site-*
 sudo rm -f ./site.php
 sudo rm -f ./modules/*/config/module_config_*.php
