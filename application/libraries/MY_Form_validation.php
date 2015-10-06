adf49db6a897060c12f922e64f50dc0475d815ae
goFrendiAsgard <gofrendiasgard@gmail.com>
2015-10-06 18:18:51 +0700

    fix unique validation

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -16,14 +16,16 @@ class MY_Form_validation extends CI_Form_validation
     public function is_unique($str, $field)
 	{
 		sscanf($field, '%[^.].%[^.]', $table, $field);
-        return ($this->CI->db->limit(1)->get_where($table, array($field => $str))->num_rows() === 0);
-        log_message('error', $table.' '.$field.' '.$str);
-        log_message('error', print_r(array(
-            $this->CI->db,
-            $this->CI->db->limit(1)->get_where($table, array($field => $str))->num_rows(),
-        ), TRUE));
-		return isset($this->CI->db)
+        log_message('error', $this->CI->db? 'yup': 'yek');
+         // bypass by gofrendi because isset is not working
+        return $this->CI->db
+ 			? ($this->CI->db->limit(1)->get_where($table, array($field => $str))->num_rows() === 0)
+ 			: FALSE;
+         // TODO: why isset is not working?
+        /*
+        return isset($this->CI->db)
 			? ($this->CI->db->limit(1)->get_where($table, array($field => $str))->num_rows() === 0)
 			: FALSE;
+        */
 	}
 }
