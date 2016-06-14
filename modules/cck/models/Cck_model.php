ae07c8d4bffe6002f3332883a9a7b6d240e68c56
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2016-06-14 08:49:25 +0700

    fix up things

--- modules/cck/models/Cck_model.php
+++ modules/cck/models/Cck_model.php
@@ -10,4 +10,20 @@ class Cck_model  extends CMS_Model{
     public function __construct(){
         parent::__construct();
     }
+
+    public function get_view_and_input_by_template($id_template){
+
+    }
+
+    public function get_default_per_record_view($id_entity){
+
+    }
+
+    public function get_field_view($id_field){
+
+    }
+
+    public function get_field_input($id_field){
+
+    }
 }
