2e747c974aede41642e27c55a211258598b3ae3a
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2016-10-07 16:13:10 +0700

    fix portfolio template

--- modules/portfolio/config/module_config.php
+++ modules/portfolio/config/module_config.php
@@ -6,35 +6,35 @@ $config['module_prefix']        = 'portfolio';
 $config['portfolio_record_template'] ='<div id="record_{{ record:id }}" class="record_container panel panel-default">'.PHP_EOL.
     '<div class="panel-body">'.PHP_EOL.
     ''.PHP_EOL.
-    '<table class="table">'.PHP_EOL.
-    '<tbody>'.PHP_EOL.
+    '   <table class="table">'.PHP_EOL.
+    '       <tbody>'.PHP_EOL.
     ''.PHP_EOL.
-    '<tr>'.PHP_EOL.
-    '    <td class="row col-md-5" rowspan="3">'.PHP_EOL.
-    '        {{ record:image }}'.PHP_EOL.
+    '           <tr>'.PHP_EOL.
+    '               <td class="row col-md-5" rowspan="3">'.PHP_EOL.
+    '                   {{ record:image_component }}'.PHP_EOL.
     ''.PHP_EOL.
-    '        <div class="col-md-12" style="padding-top:10px;">'.PHP_EOL.
-    '        {{ record:url }}'.PHP_EOL.
-    '        <a target="blank" class="btn btn-primary" href="{{ MODULE_BASE_URL }}assets/uploads/{{ record:image }}"><i class="glyphicon glyphicon-image"></i> Show</a>'.PHP_EOL.
-    '        </div>'.PHP_EOL.
+    '                   <div class="col-md-12" style="padding-top:10px;">'.PHP_EOL.
+    '                       {{ record:url }}'.PHP_EOL.
+    '                       {{ record:image_link }}'.PHP_EOL.
+    '                   </div>'.PHP_EOL.
     ''.PHP_EOL.
-    '    </td>'.PHP_EOL.
-    '    <th class="col-md-3">Name</th>'.PHP_EOL.
-    '    <td class="col-md-4">{{ record:name }}</td>'.PHP_EOL.
-    '</tr>'.PHP_EOL.
-    '<tr>'.PHP_EOL.
-    '    <th>Category</th>'.PHP_EOL.
-    '    <td>{{ record:category_name }}</td>'.PHP_EOL.
-    '</tr>'.PHP_EOL.
-    '<tr>'.PHP_EOL.
-    '    <th>Description</th>'.PHP_EOL.
-    '    <td>{{ record:description }}</td>'.PHP_EOL.
-    '</tr>'.PHP_EOL.
+    '               </td>'.PHP_EOL.
+    '               <th class="col-md-3">Name</th>'.PHP_EOL.
+    '               <td class="col-md-4">{{ record:name }}</td>'.PHP_EOL.
+    '           </tr>'.PHP_EOL.
+    '           <tr>'.PHP_EOL.
+    '               <th>Category</th>'.PHP_EOL.
+    '               <td>{{ record:category_name }}</td>'.PHP_EOL.
+    '           </tr>'.PHP_EOL.
+    '           <tr>'.PHP_EOL.
+    '               <th>Description</th>'.PHP_EOL.
+    '               <td>{{ record:description }}</td>'.PHP_EOL.
+    '           </tr>'.PHP_EOL.
     ''.PHP_EOL.
-    '</tbody>'.PHP_EOL.
-    '</table>'.PHP_EOL.
-    '<div class="edit_delete_record_container pull-right">{{ backend_urls }}</div>'.PHP_EOL.
+    '       </tbody>'.PHP_EOL.
+    '   </table>'.PHP_EOL.
+    '   <div class="edit_delete_record_container pull-right">{{ backend_urls }}</div>'.PHP_EOL.
     ''.PHP_EOL.
-    '<div style="clear:both;"></div>'.PHP_EOL.
+    '   <div style="clear:both;"></div>'.PHP_EOL.
     '</div>'.PHP_EOL.
     '</div>';
--- modules/portfolio/views/Browse_portfolio_partial_view.php
+++ modules/portfolio/views/Browse_portfolio_partial_view.php
@@ -8,5 +8,15 @@ $config = array(
         'allow_delete'    => $have_delete_privilege && $allow_navigate_backend,
     );
 foreach($result as $record){
+    if($record->image != ''){
+        $record->image_component = '<img class="col-xs-12" src="{{ MODULE_BASE_URL }}assets/uploads/'.$record->image.'" />';
+        $record->image_link = '<a target="blank" class="btn btn-primary" href="{{ MODULE_BASE_URL }}assets/uploads/'.$record->image.'"><i class="glyphicon glyphicon-image"></i> Show</a>';
+    }else{
+        $record->image_component = '';
+        $record->image_link = '';
+    }
+    if($record->url != ''){
+        $record->url = '<a href="'.$record->url.'" class="btn btn-default">Visit</a>';
+    }
     echo parse_record($record, $config);
 }
