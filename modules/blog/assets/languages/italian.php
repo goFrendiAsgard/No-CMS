90f7946ada468038fc1bdb53aa4baf581e058fc6
Alfredo Cosco <alfredo.cosco@gmail.com>
2016-11-04 23:19:18 +0100

    Italian loc for Blog Module

---
+++ modules/blog/assets/languages/italian.php
@@ -0,0 +1,8 @@
+<?php
+$lang['Manage Article'] = 'Articoli';
+$lang['Manage Category'] = 'Categorie';
+$lang['Featured Articles'] = 'In evidenza';
+$lang['Newest Articles'] = 'Articoli recenti';
+$lang['Popular Articles'] = 'Articoli popolari';
+$lang['Article Categories'] = 'Categorie articoli';
+$lang['Archive'] = 'Archivio news';
