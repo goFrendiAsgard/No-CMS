44136889ed5d307604baa2bdb124717a5281b7ea
garkok <miseryoflife@hotmail.com>
2016-11-07 10:27:42 +0300

    Added general Turkish Language translation to no-cms

---
+++ assets/nocms/languages/turkish.php
@@ -0,0 +1,120 @@
+<?php
+/*
+* lang
+* turkish=türkçe, UTF-8
+*/ 
+$lang['language_alias'] = 'tr';
+$lang['Complete Menu'] = 'Yönetici';
+$lang['Home'] = 'Anasayfa';
+$lang['Language'] = 'Dil';
+$lang['Third Party Authentication'] = 'Üçüncü Parti Kimlik Doğrulama';
+$lang['No third party authentication enabled'] = 'Üçüncü Parti Kimlik Doğrulama aktif değildir';
+$lang['Login'] = 'Giriş';
+$lang['Logout'] = 'Çıkış';
+$lang['Identity'] = 'Kimlik';
+$lang['identity'] = 'Kullanıcı adı';
+$lang['Password'] = 'Şifre';
+$lang['password'] = 'şifre';
+$lang['Forgot Password'] = 'Şifre unuttum';
+$lang['Register'] = 'Kayıt';
+$lang['No-CMS User Guide'] = 'Kullanıcı Kılavuzu';
+$lang['Change Profile'] = 'Profil Değiştir';
+$lang['CMS Management'] = 'CMS Yönetimi';
+$lang['Group Management'] = 'Group Yönetimi';
+$lang['User Management'] = 'Kullanıcı Yönetimi';
+$lang['Privilege Management'] = 'Hak Yönetimi';
+$lang['Navigation Management'] = 'Navigasyon Yönetimi';
+$lang['Widget Management'] = 'Widget Yönetimi';
+$lang['Module Management'] = 'Modül Yönetimi';
+$lang['Layout Management'] = 'Sayfa Düzeni Yönetimi';
+$lang['Change Theme'] = 'Tema Değiştir';
+$lang['Quick Link Management'] = "Hızlı Link'ler";
+$lang['Configuration Management'] = 'Ayarlar Yönetimi';
+$lang['User Info'] = 'Kullanıcı Bilgisi';
+$lang['Share This Page !!'] = 'Bu Sayfayı Paylaş!!';
+$lang['Donate No-CMS'] = "No-CMS'e Bağış!!";
+$lang['Welcome'] = 'Merhaba';
+$lang['Username already exists'] = 'Kullanıcı adı zaten vardır';
+$lang['Username is empty'] = 'Kullanıcı adı boş';
+$lang['Static Accessories'] = 'Statik Araçlar';
+$lang['Static Accesorries Widgets'] = "Statik Araçlar Widget'ları";
+$lang['Manage Slide'] = 'Slaytları Ayarla';
+$lang['Manage Tab Content'] = 'Tab İçeriğini Ayarla';
+$lang['Manage Visitor Counter'] = 'Ziyaretçi Sayacını Ayarla';
+$lang['Contact Us'] = 'Bize Ulaşın';
+$lang['Manage Message'] = 'Mesajları Yönet';
+$lang['Toggle Edit'] = 'Düzenleme Modu';
+$lang['Toggle View'] = 'Görüntüleme Modu';
+$lang['Delete Selected'] = 'Seçilen(leri) Kaldır';
+$lang['Add Group'] = 'Grup Ekle';
+$lang['Name'] = 'isim';
+$lang['Content'] = 'İçerik';
+$lang['Group Name'] = 'Grup Adı';
+$lang['Description'] = 'Açıklaması';
+$lang['Factory Reset'] = 'Fabrika Ayarları';
+$lang['Edit'] = 'Düzenle';
+
+$lang['Navigation'] = 'Navigasyon';
+$lang['Quicklink'] = 'Hızlı Link\'ler';
+$lang['Top Section'] = 'Üst Bölüm';
+$lang['Changes applied'] = 'Değişiklikler kaydedildi';
+$lang['Appearance'] = 'Görünüm';
+$lang['Email settings'] = 'Email ayarları';
+$lang['Images'] = 'Görseller';
+
+$lang['Or Login with']							= 'veya';
+$lang['email or username']						= 'email veya kullanıcı adı';
+$lang['Feature Disabled']						= 'Özellik kapalıdır';
+
+$lang['New Password']							= 'Yeni Şifre';
+$lang['New Password (Again)']					= 'Yeni Şifre (Tekrar)';
+
+$lang['No Preview']								= 'Ön Gösterim Yoktur';
+$lang['Theme is in use']						= 'Tema kullanımdadır';
+$lang['Click to use this theme']				= 'Bu temayı kullanmak için tıklayınız';
+$lang['Error']									= 'Hata';
+
+$lang['Here you can edit your user profile. Click the button once you\'ve finish editing your user profile']	= 'Burada kullanıcı profilinizi düzeltebilirsiniz. Kullanıcı Profilinizi düzeltmeyi bitirince lütfen tuşa basınız';
+$lang['Password is empty']						= 'Şifre boş';
+$lang['Confirm password doesn\'t match']		= 'Şifre girişleri birbiri ile aynı değildir';
+$lang['Profile Picture']						= 'Profil Resmi';
+$lang['User Name']								= 'Kullanıcı İsmi';
+$lang['Real Name']								= 'Gerçek İsim';
+$lang['Birthdate']								= 'Doğumgünü';
+$lang['Not Set']								= 'Belirlenmedi';
+$lang['Language']								= 'Dil';
+$lang['Theme']									= 'Tema';
+$lang['Self Description']						= 'Hakkınızda';
+$lang['Change Password']						= 'Şifre Değiştir';
+$lang['Confirm Password']						= 'Şifre (Tekrar/Doğrulama)';
+
+$lang['Name *']									= 'İsim *';
+$lang['Your message has been sent']				= 'Mesajınız gönderildi';
+$lang['Your Name']								= 'İsminiz';
+$lang['Email *']								= 'Email *';
+$lang['Your Email']								= 'Email Adresiniz';
+$lang['Message *']								= 'Mesaj *';
+$lang['Your Message']							= 'Mesajınız';
+$lang['Send']									= 'Gönder';
+$lang['* indicates a required field']			= '* (yıldız) tamamlanması gereken giriş belirtir';
+$lang['Setting']								= 'Ayarlar';
+$lang['Table Prefix']							= 'Tablo Ön Adı (tbl_ gibi)';
+$lang['Module Prefix']							= 'Modül Ön Adı (mdl_ gibi)';
+$lang['Save Setting']							= 'Ayarları Kaydet';
+$lang['No setting available']					= 'Ayar Yoktur';
+$lang['Back to module management']				= 'Modül Yönetimi\'ne Geri Dön';
+$lang['Delete Selected']						= 'Seçileni Sil';
+$lang['Selected row deleted']					= 'Seçilmiş olan satır silindi';
+
+$lang['Please provide your email address']		= 'Lütfen e-mail adresinizi belirtiniz';
+$lang['No third party authentication enabled']	= 'Üçüncü Parti Kimlik doğrulama aktif değildir';
+
+$lang['Send activation code to my email']		= 'Aktivasyon kodunu e-mailime gönder';
+
+
+$lang['Selected row deleted']					= 'Seçilmiş olan satır silindi';
+
+/* 404 Page Translation */
+$lang['404 Page not found']						= '404 Sayfa Bulunamadı';
+$lang['Sorry, the page does not exists.']		= 'Özür dileriz, aradığınız sayfa yoktur';
+$lang['Please go back']							= 'Lütfen geri gidiniz';
