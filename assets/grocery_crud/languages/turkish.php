feb1dbdcdbd712e5210b8c899030bb31e34405e4
garkok <miseryoflife@hotmail.com>
2016-11-07 10:27:22 +0300

    Added minor language improvements for Turkish

--- assets/grocery_crud/languages/turkish.php
+++ assets/grocery_crud/languages/turkish.php
@@ -1,8 +1,8 @@
 <?php  
 /* Translated by Sinan Eldem - www.sinaneldem.com.tr */
-	$lang['list_add'] 				= 'Ekle';
+	$lang['list_add'] 					= 'Ekle';
 	$lang['list_actions'] 				= 'İşlemler';
-	$lang['list_page'] 				= 'Sayfa';	
+	$lang['list_page'] 					= 'Sayfa';	
 	$lang['list_paging_of'] 			= '#';
 	$lang['list_displaying']			= 'Toplam {results} kayıttan {start} ile {end} arası gösteriliyor';
 	$lang['list_filtered_from']			= '{total_results} toplam kayıttan filtrelendi';
@@ -11,35 +11,35 @@
 	$lang['list_zero_entries']			= '0 kayıttan 0 ile 0 arası gösteriliyor';
 	$lang['list_search'] 				= 'Arama';	
 	$lang['list_search_all'] 			= 'Tümünü ara';
-	$lang['list_clear_filtering'] 			= 'Filtreyi temizle';
+	$lang['list_clear_filtering'] 		= 'Filtreyi temizle';
 	$lang['list_delete'] 				= 'Sil';
-	$lang['list_edit'] 				= 'Düzenle';	
+	$lang['list_edit'] 					= 'Düzenle';	
 	$lang['list_paging_first'] 			= 'İlk';
-	$lang['list_paging_previous'] 			= 'Önceki';
+	$lang['list_paging_previous'] 		= 'Önceki';
 	$lang['list_paging_next'] 			= 'Sonraki';
 	$lang['list_paging_last'] 			= 'Son';
 	$lang['list_loading'] 				= 'Yükleniyor...';	
 
-	$lang['form_edit'] 				= 'Düzenle';
+	$lang['form_edit'] 					= 'Düzenle';
 	$lang['form_back_to_list'] 			= 'Listeye geri dön';
-	$lang['form_update_changes'] 			= 'Değişiklikleri uygula';
+	$lang['form_update_changes'] 		= 'Değişiklikleri uygula';
 	$lang['form_cancel'] 				= 'İptal';	
-	$lang['form_update_loading'] 			= 'Yükleniyor, değişliklikler uygulanıyor.';
-	$lang['update_success_message'] 		= 'Veri başarıyla güncellendi.';	
-	$lang['form_go_back_to_list'] 			= 'Listeye geri dön';
+	$lang['form_update_loading'] 		= 'Yükleniyor, değişliklikler uygulanıyor.';
+	$lang['update_success_message'] 	= 'Veri başarıyla güncellendi.';	
+	$lang['form_go_back_to_list'] 		= 'Listeye geri dön';
 	
-	$lang['form_add'] 				= 'Ekle';
-	$lang['insert_success_message'] 		= 'Veri başarıyla veritabanına kaydedildi.';
-	$lang['form_or']				= 'veya';
-	$lang['form_save'] 				= 'Kaydet';
-	$lang['form_insert_loading'] 			= 'Yükleniyor, veri kaydediliyor...';
+	$lang['form_add'] 					= 'Ekle';
+	$lang['insert_success_message'] 	= 'Başarıyla veritabanına kaydedildi.';
+	$lang['form_or']					= 'veya';
+	$lang['form_save'] 					= 'Kaydet';
+	$lang['form_insert_loading'] 		= 'Yükleniyor, veri kaydediliyor...';
 	
-	$lang['form_upload_a_file'] 			= 'Dosya yükle';
-	$lang['form_upload_delete'] 			= 'sil';
+	$lang['form_upload_a_file'] 		= 'Dosya yükle';
+	$lang['form_upload_delete'] 		= 'Sil';
 	$lang['form_button_clear'] 			= 'Temizle';	
 	
-	$lang['delete_success_message'] 		= 'Kayıt başarıyla veritabanından kaldırıldı.';
-	$lang['delete_error_message'] 			= 'Kayıt veritabanından kaldırılamadı.';
+	$lang['delete_success_message'] 	= 'Başarıyla veritabanından kaldırıldı.';
+	$lang['delete_error_message'] 		= 'Kayıt veritabanından kaldırılamadı.';
 	
 	/* Javascript messages */
 	$lang['alert_add_form']				= 'Girmiş olduğunuz veri eklenemedi veya kaydedilemedi.\\nListeye geri dönmek istediğinizden emin misiniz?';
@@ -58,14 +58,14 @@
 	$lang['form_active']				= 'Aktif';
 	
 	/* Added in version 1.2.2 */
-	$lang['form_save_and_go_back']			= 'Kaydet ve Listeye Geri Dön';
-	$lang['form_update_and_go_back']		= 'Güncelle ve Listeye Geri Dön';
+	$lang['form_save_and_go_back']		= 'Kaydet ve Listeye Geri Dön';
+	$lang['form_update_and_go_back']	= 'Güncelle ve Listeye Geri Dön';
 
 	/* Upload functionality */
 	$lang['string_delete_file'] 			= "Dosya Siliniyor";
-	$lang['string_progress'] 			= "İlerleme: ";
+	$lang['string_progress'] 				= "İlerleme: ";
 	$lang['error_on_uploading'] 			= "Dosya yüklenirken bir hata oluştu.";
-	$lang['message_prompt_delete_file'] 		= "Bu dosyayı silmek istediğinize emin misiniz?";
+	$lang['message_prompt_delete_file'] 	= "Bu dosyayı silmek istediğinize emin misiniz?";
 	
 	$lang['error_max_number_of_files']		= "Her seferinde sadece bir dosya yükleyebilirsiniz.";
 	$lang['error_accept_file_types']		= "Bu uzantıda dosya yüklemeye yetkiniz yoktur.";
@@ -74,18 +74,18 @@
 
 	/* Added in version 1.3.1 */
 	$lang['list_export']				= "Dışarı Aktar";
-	$lang['list_print']				= "Yazdır";
+	$lang['list_print']					= "Yazdır";
 	$lang['minimize_maximize']			= 'Küçült/Büyüt';
 
 
 	/* Added in version 1.4 */
-	$lang['list_view']				= 'Görüntüle';
+	$lang['list_view']					= 'Görüntüle';
 
 	/* Added in version 1.5.1 */
-	$lang['ui_day'] = 'dd';
-	$lang['ui_month'] = 'mm';
-	$lang['ui_year'] = 'yyyy';
+	$lang['ui_day'] 					= 'dd';
+	$lang['ui_month'] 					= 'mm';
+	$lang['ui_year'] 					= 'yyyy';
 
 	/* Added in version 1.5.2 */
-	$lang['list_more'] = 'More';
+	$lang['list_more'] 					= 'Daha Fazla';
 
